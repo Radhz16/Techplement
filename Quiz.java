1c69644ddc08c08a35bba54aafa2f616329cca47
Radhz16 <120027842+Radhz16@users.noreply.github.com>
2024-07-01 21:07:18 +0530

    Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,39 +1,89 @@
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+
+class Question {
+    private String question;
+    private String[] choices;
+    private int answer;
+
+    public Question(String question, String[] choices, int answer) {
+        this.question = question;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getChoices() {
+        return choices;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+}
+
+
 public class Quiz {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    private List<Question> questions;
+    private Scanner scan;
+
+    public Quiz() {
+        questions = new ArrayList<>();
+        scan = new Scanner(System.in);
+    }
+
+    public void addQuestion(String question, String[] choices, int answer) {
+        Question newQuestion = new Question(question, choices, answer);
+        questions.add(newQuestion);
+    }
+
+    public void startQuiz() {
         int score = 0;
 
-        //Questions
-        String[] questions = {"Who is the father of atomic bomb?\n",
-            "How many continents are there?\n","Which is the largest ocean?\n","What is the capital of India?\n"};
+        for (int i = 0; i < questions.size(); i++) {
+            Question currentQuestion = questions.get(i);
 
-        //Options
-        String[] options = {"1.Issac Newton\t 2.Albert Einstein\t 3.Robert Hooke\t 4.Robert Oppenheimer\t",
-            "1.10\t 2.7\t 3.6\t 4.6\t", "1.Pacific Ocean\t 2.Indian Ocean\t 3.Atlantic Ocean\t 4.Arctic Ocean\t",
-            "1.Madrid\t 2.Rome\t 3.New Delhi\t 4.Kavarati\t"};
-
-        //Answers
-        String[] answers = {"4","2","1","3"};
-        System.out.println("Greetings Everyone , Welcome to the Quiz!");
-        System.out.println("Here are your questions...");
-        
-        for (int i = 0 ; i < questions.length ; i++){
-            System.out.println(questions[i]);
-            System.out.println(options[i]);
-            System.out.println("Enter your answer: ");
-            String answer = scan.nextLine();
-            if (answer.equals(answers[i])){
-                System.out.println("Correct !");
-                score++;
-            } else {
-                System.out.println("Incorrect !");
+            System.out.println("Question " + (i + 1) + ": " + currentQuestion.getQuestion());
+            String[] choices = currentQuestion.getChoices();
+            for (int j = 0; j < choices.length; j++) {
+                System.out.println((j + 1) + ": " + choices[j]);
             }
 
+            System.out.print("Enter your answer : ");
+            int userAnswer = scan.nextInt()-1 ;
+
+            if (userAnswer == currentQuestion.getAnswer()){
+                System.out.println("Correct :)");
+                score++;
+            } else {
+                System.out.println("Incorrect :( . The correct answer is "+ choices[currentQuestion.getAnswer()]);
+            }
         }
-        System.out.println("Quiz ended . You got "+score+"/"+questions.length);
+        if ( score ==questions.size()){
+            System.out.println("Congratulations ! You got "+score+"/"+questions.size());
+        } else if (score== questions.size() -1){
+            System.out.println("You were so close! Your score is "+score+"/"+questions.size());
+        } else {
+            System.out.println("Need to improve . You got "+score+"/"+questions.size());
+        }
+    }
+
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+
+      
+        quiz.addQuestion("Which is the largest ocean in the world?", new String[]{"Atlantic", "Pacific", "Arctic", "Indian"}, 1);
+        quiz.addQuestion("Which country won T20 World Cup in 2024?", new String[]{"India", "Austraila", "South Africa", "England"}, 0);
+        quiz.addQuestion("How many continents are there in the world?", new String[]{"10", "5", "7", "6"}, 2);
+        quiz.addQuestion("Which country won the FIFA World Cup in 2018?", new String[]{"France", "Brazil", "Germany", "Argentina"}, 0);
+
+      
+        quiz.startQuiz();
     }
 }
 
